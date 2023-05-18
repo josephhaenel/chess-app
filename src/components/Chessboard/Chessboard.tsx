81f45e15cc019a24cebc44b38190e31f14712d94
Joe <josephhaenel@gmail.com>
2023-05-18 14:13:52 -0500

    added grid snapping

--- src/components/Chessboard/Chessboard.tsx
+++ src/components/Chessboard/Chessboard.tsx
@@ -1,6 +1,7 @@
 import './Chessboard.css';
 import Tile from '../Tile/Tile';
 import * as React from 'react';
+import Referee from "./refree/Referee";
 
 
 const horizontalAxist = ["a", "b", "c", "d", "e", "f", "g", "h"];
@@ -12,51 +13,54 @@ interface Piece {
     y: number;
 }
 
-const pieces: Piece[] = [];
+// const pieces: Piece[] = [];
 
-for( let p = 0; p < 2; p++ ) {
+const initialBoardState: Piece[] = [];
+
+for (let p = 0; p < 2; p++) {
     const type = (p === 0) ? "b" : "w";
     const y = (p === 0) ? 7 : 0;
 
-    pieces.push({ image: `assets/images/rook_${type}.png`, x: 0, y })
-    pieces.push({ image: `assets/images/rook_${type}.png`, x: 7, y })
-    pieces.push({ image: `assets/images/knight_${type}.png`, x: 1, y })
-    pieces.push({ image: `assets/images/knight_${type}.png`, x: 6, y })
-    pieces.push({ image: `assets/images/bishop_${type}.png`, x: 2, y })
-    pieces.push({ image: `assets/images/bishop_${type}.png`, x: 5, y })
-    pieces.push({ image: `assets/images/king_${type}.png`, x: 4, y })
-    pieces.push({ image: `assets/images/queen_${type}.png`, x: 3, y })
-}
-
-for(let i = 0; i < 8; i++) {
-    pieces.push({ image: "assets/images/pawn_b.png", x: i, y: 6 });
+    initialBoardState.push({ image: `assets/images/rook_${type}.png`, x: 0, y })
+    initialBoardState.push({ image: `assets/images/rook_${type}.png`, x: 7, y })
+    initialBoardState.push({ image: `assets/images/knight_${type}.png`, x: 1, y })
+    initialBoardState.push({ image: `assets/images/knight_${type}.png`, x: 6, y })
+    initialBoardState.push({ image: `assets/images/bishop_${type}.png`, x: 2, y })
+    initialBoardState.push({ image: `assets/images/bishop_${type}.png`, x: 5, y })
+    initialBoardState.push({ image: `assets/images/king_${type}.png`, x: 4, y })
+    initialBoardState.push({ image: `assets/images/queen_${type}.png`, x: 3, y })
 }
 
 for (let i = 0; i < 8; i++) {
-    pieces.push({ image: "assets/images/pawn_w.png", x: i, y: 1 });
+    initialBoardState.push({ image: "assets/images/pawn_b.png", x: i, y: 6 });
 }
 
+for (let i = 0; i < 8; i++) {
+    initialBoardState.push({ image: "assets/images/pawn_w.png", x: i, y: 1 });
+}
 
 export default function Chessboard() {
+    const [activePiece, setActivePiece] = React.useState<HTMLElement | null >(null);
+    const [gridX, setGridX] = React.useState(0);
+    const [gridY, setGridY] = React.useState(0);
+    const [pieces, setPieces] = React.useState<Piece[]>(initialBoardState);
 
     const chessBoardRef = React.useRef<HTMLDivElement>(null);
 
-    let activePiece: HTMLElement | null = null;
-
     function grabPiece(e: React.MouseEvent) {
+        const chessboard = chessBoardRef.current;
         const element = e.target as HTMLElement;
-        if (element.classList.contains("chess-piece")) {
-            console.log(e.target);
+        if (element.classList.contains("chess-piece") && chessboard) {
+            setGridX(Math.floor((e.clientX - chessboard.offsetLeft) / 100));
+            setGridY(Math.abs(Math.ceil((e.clientY - chessboard.offsetTop - 800) / 100)));
 
             const x = e.clientX - 50;
-
             const y = e.clientY - 50;
-
             element.style.position = "absolute";
             element.style.left = `${x}px`;
             element.style.top = `${y}px`;
 
-            activePiece = element;
+            setActivePiece(element);
         }
     }
 
@@ -97,8 +101,24 @@ export default function Chessboard() {
     }
 
     function dropPiece(e: React.MouseEvent) {
-        if (activePiece) {
-            activePiece = null;
+        const chessboard = chessBoardRef.current;
+        if (activePiece && chessboard) {
+            const x = Math.floor((e.clientX - chessboard.offsetLeft) / 100);
+            const y = Math.abs(Math.ceil((e.clientY - chessboard.offsetTop - 800) / 100));
+
+
+            setPieces((value) => {
+                const pieces = value.map((p) => {
+                    if(p.x === gridX && p.y === gridY) {
+                        p.x = x;
+                        p.y = y;
+                    }
+                    return p;
+                });
+                return pieces;
+            });
+
+            setActivePiece(null);
         }
     }
 
---
+++ src/referee/referee.ts
@@ -0,0 +1,6 @@
+export default class Referee {
+    isValidMove() {
+        console.log("Referee is checking move")
+        return true;
+    }
+}
